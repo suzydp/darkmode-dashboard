e54c14f479b59a752608fa127c547d08295afed8
Kaai Suzuki <suzydp@me.com>
2019-10-04 23:05:53 -0700

    Single File Components - Routing in Vue with Create about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
+    <Header />
     <h1>This is an about page</h1>
   </div>
 </template>
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator";
+  import Header from "@/components/Header.vue";
+
+  @Component({
+    components: {
+      Header
+    }
+  })
+  export default class About extends Vue {}
+</script>
