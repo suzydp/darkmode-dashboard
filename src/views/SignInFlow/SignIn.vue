caeeeefdf2754400892f361645d65468b9a3b6b1
Kaai Suzuki <suzydp@me.com>
2019-10-06 00:10:26 -0700

    Single File Components - Sign In page

--- src/views/SignInFlow/SignIn.vue
+++ src/views/SignInFlow/SignIn.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="signin">
-    <Header />
-    <h1>Signin section here</h1>
+  <div class="container">
+    <div class="request">
+      Don't have a Design+Code HQ account?
+      <router-link to="/request">Request an account</router-link>
+    </div>
+    <div class="login">
+      <img src="@/assets/DCHQ.svg">
+      <h4>Sign into Design+Code HQ</h4>
+      <input type="email" placeholder="Email">
+      <input type="password" placeholder="Password">
+      <button>Sign In</button>
+      <router-link to="/recover">Forgot your password?</router-link>
+    </div>
   </div>
 </template>
 <script lang="ts">
