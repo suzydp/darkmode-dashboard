6a7197ee78ba98221c439d4fa769ea2f6996bb4b
Kaai Suzuki <suzydp@me.com>
2019-10-06 17:11:01 -0700

    vue-darkmode-2 - Add Mutations

--- src/store.ts
+++ src/store.ts
@@ -3,8 +3,35 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+// Init State
+const state = {
+  isDarkMode: true
+}
+
+// Init getters
+const getters = {
+  isDarkMode() {
+    return state.isDarkMode
+  }
+}
+
+// Init Mutations - mutations are group of functions which change the state.
+const mutations = {
+  toggleDarkMode() {
+    // if darkmode was enable, we gotta toggle darkmode to false
+    if(state.isDarkMode === true) {
+      state.isDarkMode = false;
+      document.body.style.background = "212c4f";
+    } else {
+      state.isDarkMode = true;
+      document.body.style.background = "f0f3f5";
+    }
+  }
+}
+
 export default new Vuex.Store({
-  state: {},
+  state: state,
+  getters: getters,
   mutations: {},
   actions: {}
 });
